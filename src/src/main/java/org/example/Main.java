2a5303864d7185cfea6d1b59f168d5187cfc2ced
joao pedro <j.lazzarotto@aluno.senai.br>
2026-06-12 21:59:13 -0300

    uso de classes externas

---
+++ src/src/main/java/org/example/Main.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+
+        pessoa.setNome("João");
+        pessoa.setIdade(25);
+        pessoa.setAltura(1.75f);
+        pessoa.setSalario(3000.50);
+        pessoa.setEstudante(true);
+
+        System.out.println("Nome: " + pessoa.getNome());
+        System.out.println("Idade: " + pessoa.getIdade());
+        System.out.println("Altura: " + pessoa.getAltura());
+        System.out.println("Salário: " + pessoa.getSalario());
+        System.out.println("Estudante: " + pessoa.isEstudante());
+    }
+}
---
+++ src/src/main/java/org/example/Pessoa.java
@@ -0,0 +1,53 @@
+package org.example;
+
+public class Pessoa {
+
+    private String Nome;
+    private int Idade;
+    private float Altura;
+
+    private double Salario;
+
+    private boolean Estudante;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public int getIdade() {
+        return Idade;
+    }
+
+    public float getAltura() {
+        return Altura;
+    }
+
+    public double getSalario() {
+        return Salario;
+    }
+
+    public boolean isEstudante() {
+        return Estudante;
+    }
+
+    public void setNome(String Nome) {
+        this.Nome = Nome;
+    }
+
+    public void setIdade(int Idade) {
+        this.Idade = Idade;
+    }
+
+    public void setAltura(float Altura) {
+        this.Altura = Altura;
+    }
+
+    public void setSalario(double Salario) {
+        this.Salario = Salario;
+    }
+
+    public void setEstudante(boolean Estudante) {
+        this.Estudante = Estudante;
+    }
+
+}
